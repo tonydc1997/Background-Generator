7a3f418765ee0b6645887574794aaa52ba292b77
Anthony Curtis <tonydc1997@gmail.com>
2018-04-11 08:06:10 -0400

    Begin event listeners

---
+++ script.js
@@ -0,0 +1,12 @@
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+
+
+color1.addEventListener("input", () => {
+  console.log(color1);
+})
+
+color2.addEventListener("input", () => {
+  console.log(color2);
+})
