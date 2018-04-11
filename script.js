1c94ebada7aea494440d48af2f4ea547d5ba4623
Anthony Curtis <tonydc1997@gmail.com>
2018-04-11 09:56:43 -0400

    Add comments for future modification

--- script.js
+++ script.js
@@ -17,3 +17,9 @@ function setGradient() {
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+// 1. Write code so that the colour inputs match the background generated on the first page load. 
+
+// 2. Display the initial CSS linear gradient property on page load.
+
+// 3. BONUS: Add a random button which generates two random numbers for the colour inputs.
