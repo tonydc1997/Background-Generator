0314b13cc9055fb69f7eb9e239eac5f693d1cff7
Anthony Curtis <tonydc1997@gmail.com>
2018-04-11 09:52:32 -0400

    Finish initial background generator functionality

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const css = document.querySelector("h3");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
 
+function setGradient() {
+  body.style.background = 
+    "linear-gradient(to right, " 
+    + color1.value 
+    + ", " 
+    + color2.value 
+    + ")";
+}
 
-color1.addEventListener("input", () => {
-  console.log(color1);
-})
+color1.addEventListener("input", setGradient);
 
-color2.addEventListener("input", () => {
-  console.log(color2);
-})
+color2.addEventListener("input", setGradient);
